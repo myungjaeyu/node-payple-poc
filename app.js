55b2f96c2f86b335357b67b2430a4cf734017318
myungjaeyu <rootcode10@gmail.com>
2018-07-27 18:11:11 +0900

    Added json-dotenv to app.

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 
+require('json-dotenv')()
+
 
 const app = express()
 
@@ -8,5 +10,29 @@ const app = express()
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
+
+app.post('/auth', (req, res) => {
+
+    let url = 'https://testcpay.payple.kr/php/auth.php',
+        params = {
+            cst_id  : process.env.CST_ID || '',
+            custKey : process.env.CUST_KEY || ''
+        }
+
+
+    res.json({ url, params })
+    /**
+     * return json
+    {
+        "url": "https://testcpay.payple.kr/php/auth.php",
+        "params": {
+            "cst_id": "test",
+            "custKey": "abcd1234567890"
+        }
+    }
+    */
+
+
+})
 
 module.exports = app
