668d4a1b12f37b931cc833d941f4e1c76d4eb363
myungjaeyu <rootcode10@gmail.com>
2018-07-27 18:17:19 +0900

    Added express.static middleware.

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const app = express()
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.static(`${__dirname}/dist`))
 
 
 app.post('/auth', (req, res) => {
