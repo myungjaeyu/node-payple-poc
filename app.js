1aa9d64a64687396fb5c0ccdb05fc1c3140e81f8
myungjaeyu <rootcode10@gmail.com>
2018-07-27 18:15:17 +0900

    Added Payple auth api.
    - https://github.com/PAYPLECORP/manual#가맹점-인증

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const { get } = require('axios')
 
 require('json-dotenv')()
 
@@ -20,18 +21,20 @@ app.post('/auth', (req, res) => {
         }
 
 
-    res.json({ url, params })
+    get(url, { params })
+        .then(r => res.json( {  ...r.data }))
+        .catch(err => console.error(err))
     /**
      * return json
     {
-        "url": "https://testcpay.payple.kr/php/auth.php",
-        "params": {
-            "cst_id": "test",
-            "custKey": "abcd1234567890"
-        }
+        "result": "success",
+        "result_msg": "사용자 인증 완료!!",
+        "cst_id": "test",
+        "custKey": "abcd1234567890",
+        "AuthKey": "fc63fe1ed0016321a666b1a9c0b6f68d9d5e4ff5c57e448314ba3352da59f1a7",
+        "return_url": "https://testcpay.payple.kr/index.php?ACT_=PAYM"
     }
     */
-
 
 })
 
